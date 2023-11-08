25c238da116e6b1ddd926b55100403fe28a0632c
R-Samsam <148309592+R-Samsam@users.noreply.github.com>
2023-11-09 00:29:31 +0700

    Create playground.py

---
+++ playground.py
@@ -0,0 +1,10 @@
+my_tuple = (1 , "Apel" , 3,14)
+print(my_tuple[0])
+
+my_list = [1 , 2 , 3 , 'wow']
+my_list[-1] = 68
+print(my_list[-1])
+
+my_dict = {"nama" : "samsam" , "gender" : "L" ,"No_tlp" : 854}
+my_dict ['nama'] = 'dodo'
+print (my_dict['nama'])
